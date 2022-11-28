4cad5bfaf3a548ed35ddec400c7003af7e77db6b
NikolayPilgun <a03198814s@gmail.com>
2022-11-28 14:51:50 +0500

    Second section

---
+++ src/components/programs/Programs.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import RightArrow from "../../assets/rightArrow.png";
+import { programsData } from "../../data/programsData";
+import styles from "./programs.module.css";
+
+function Programs() {
+	return (
+		<div className={styles.programs} id={styles.programs}>
+			{/* header */}
+			<div className={styles.programsHeader}>
+				<span className="stroke_text">Explore our</span>
+				<span>Programs</span>
+				<span className="stroke_text">to shape you</span>
+			</div>
+			<div className={styles.programCategories}>
+				{programsData.map((program, i) => (
+					<div className={styles.category} key={i}>
+						{program.image}
+						<span>{program.heading}</span>
+						<span>{program.details}</span>
+						<div className={styles.joinNow}>
+							<span>Join Now</span>
+							<img src={RightArrow} alt="RightArrow" />
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default Programs;
