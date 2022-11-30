f10b8913fc2e8a9682953fce314841e1ee860899
NikolayPilgun <a03198814s@gmail.com>
2022-11-30 18:48:56 +0500

    Seventh section

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import Logo from "../../assets/logo.png";
+import styles from "./footer.module.css";
+
+function Footer() {
+	return (
+		<footer className={styles.footer}>
+			<hr />
+			<div className={styles.footerContainer}>
+				<div className={styles.socialLinks}>
+					<img src={Github} alt="Github" />
+					<img src={Instagram} alt="Instagram" />
+					<img src={LinkedIn} alt="LinkedIn" />
+				</div>
+				<div className={styles.footerLogo}>
+					<img src={Logo} alt="Logo" />
+				</div>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
